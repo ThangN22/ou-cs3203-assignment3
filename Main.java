61287506f9909f131dbc1df42ef612c4e108b8ee
thang_nguyen <thangq.nguyen22@gmail.com>
2023-02-15 23:18:21 -0600

    creat a main class

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] arr = new int[4];
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter four numbers");
+		for(int i = 0; i < arr.length; ++i) {
+			arr[i] = scanner.nextInt();
+		}
+		scanner.close();
+		int total = sumOfArray.arraySum(arr);
+		int product = productOfArray.arrayProduct(arr);
+		System.out.print("total: " + total + System.lineSeparator() + "product: " + product);
+	}
+}
