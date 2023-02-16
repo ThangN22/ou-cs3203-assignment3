19db7aba25b9e57771129875a298f2efb9310a77
thang_nguyen <thangq.nguyen22@gmail.com>
2023-02-15 23:01:38 -0600

    create product of array function

---
+++ productOfArray.java
@@ -0,0 +1,10 @@
+
+public class productOfArray {
+	public static int arrayProduct (int[] inputArray) {
+		int total = 1;
+		for (int i = 0; i < inputArray.length; ++i) {
+			total = total * inputArray[i];
+		}
+		return total;
+	}
+}
