2db52b28e23e19da8e34457ec37872f6a14743e5
thang_nguyen <thangq.nguyen22@gmail.com>
2023-02-21 16:12:43 -0600

    create a class that reverses contents of an int array

---
+++ reverseArray.java
@@ -0,0 +1,20 @@
+
+
+
+public class reverseArray {
+	public static void reverse(int[] inputArray) {
+	    int left = 0;
+	    int right = inputArray.length - 1;
+
+	    while( left < right ) {
+
+	        int temp = inputArray[left];
+	        inputArray[left] = inputArray[right];
+	        inputArray[right] = temp;
+
+	        
+	        left++;
+	        right--;
+	    }
+	}
+}
